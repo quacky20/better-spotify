a4493f384fc14174504a92c751fd8d55ebf60e5a
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2025-05-15 11:35:15 +0530

    update login

--- betterSpotify/src/components/Login.jsx
+++ betterSpotify/src/components/Login.jsx
@@ -5,6 +5,7 @@ const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=" + clientId + "&response_type=code&redirect_uri=" + redirectUri + "&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
+console.log(AUTH_URL)
 
 function Login(){
     return(
